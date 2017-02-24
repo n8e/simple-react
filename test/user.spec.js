0abb9f8b9f8099d002b2b8879f90e3b5775070bc
n8e <godmetweenciati@gmail.com>
2017-02-24 13:07:03 +0300

    Added second spec test

--- test/user.spec.js
+++ test/user.spec.js
@@ -2,16 +2,25 @@ var request = require('superagent');
 var expect = require('expect.js');
 
 describe('GET /api/users', function() {
-  it('should get all users', function(done) {
+  it('should get response OK', function(done) {
     request
       .get('http://localhost:3000/api/users')
       .set('Accept', 'application/json')
       .end(function(err, res) {
         expect(res).to.exist;
+        expect(res.status).to.equal(200);
+        expect(res.ok).to.equal(true);
+        done();
+      });
+  });
+  it('should get all users in form of an array', function(done) {
+    request
+      .get('http://localhost:3000/api/users')
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
         expect(typeof res.body).to.equal('object');
         expect(Array.isArray(res.body)).to.equal(true);
-        expect(res.status).to.equal(200)
         done();
-      })
-  })
+      });
+  });
 })
